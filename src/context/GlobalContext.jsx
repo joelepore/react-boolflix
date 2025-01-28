51e714d47a8852cc943d8fdd3910f96494587e8e
joelepore <giuseppejoelepore@gmail.com>
2025-01-28 14:40:58 +0100

    feat: refactor GlobalContext to use a single global state object

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,15 +3,19 @@ import { createContext, useState } from "react"
 export const ContextProvider = createContext();
 
 export const GlobalContext = ({ children }) => {
-  const [movies, setMovies] = useState([]);
-  const [series, setSeries] = useState([]);
-  const [search, setSearch] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSearching, setIsSearching] = useState(false);
+  const initialGlobalState = {
+    movies: [],
+    series: [],
+    search: [],
+    isLoading: false,
+    isSearching: false,
+  }
+  const [globalState, setGlobalState] = useState(initialGlobalState);
+
   const [selectedGenre, setSelectedGenre] = useState(0);
 
   return (
-    <ContextProvider.Provider value={{ movies, series, search, isLoading, isSearching, selectedGenre, setSelectedGenre }}>
+    <ContextProvider.Provider value={{ globalState, setGlobalState, selectedGenre, setSelectedGenre }}>
       {children}
     </ContextProvider.Provider>
   )
