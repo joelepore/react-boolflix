4b9c8477d3c95bf656a2f3ea1b9f2ed1a4545768
joelepore <giuseppejoelepore@gmail.com>
2025-01-28 14:36:32 +0100

    feat(milestone 0): add global state management in GloabalContext

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,10 +1,17 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 export const ContextProvider = createContext();
 
 export const GlobalContext = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState(0);
+
   return (
-    <ContextProvider.Provider>
+    <ContextProvider.Provider value={{ movies, series, search, isLoading, isSearching, selectedGenre, setSelectedGenre }}>
       {children}
     </ContextProvider.Provider>
   )
