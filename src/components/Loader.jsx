08a45d4298e2e841b50312fef2e20fdab5d3c843
joelepore <giuseppejoelepore@gmail.com>
2025-01-29 17:00:10 +0100

    feat: add Loader component with react-loader-spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,14 @@
+import { ThreeCircles } from "react-loader-spinner"
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="h-screen flex items-center justify-center">
+
+      <ThreeCircles
+        height={80}
+        width={80}
+        color="red"
+      />
+    </div>
   )
 }
 
